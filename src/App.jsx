7868400b64d5efc34dac60fb84e60707f8aa1322
Serhiy Dvoryannikov <dvoryannikovserhiy@gmail.com>
2021-11-05 09:30:25 +0200

    added prettier and editconfig configuration file

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,5 @@ function App() {
 		</div>
 	);
 }
-//TODO
-//fix styled components, make them for props
-//add eslint, and prettier files configuration
-//create additional components and functions
-//rename files
 
 export default App;
